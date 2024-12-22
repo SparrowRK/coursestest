d702a33bcb1cbd6551a2306fd82aecf1d1e1cc65
Rostyslav <roskillrk@gmail.com>
2024-12-22 14:03:53 +0200

    courses before gitpg

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+
+
+//  удобный скролл и вверх
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1200) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href=#up]").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+});
